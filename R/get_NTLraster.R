33f28f4dc360f20d010b352874197df8a1411dd9
Vayona Narekuli <vayona.narekuli4@gmail.com>
2024-04-24 17:37:18 +0200

    get_NTLraster update

--- R/get_NTLraster.R
+++ R/get_NTLraster.R
@@ -44,7 +44,7 @@ get_NTLraster <- function(country = "NLD", bearer, product_id, ymd, low_rm = FAL
     return(r)
   }
   if (raster_to_points == TRUE) {
-    r <- rasterToPoints(r)
+    r <- rasterToPoints(r, spatial = TRUE)
     return(r)
   } else {
     return(r)
