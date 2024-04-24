d02809f09a8587d2cd4b0d1c67565625914fe63a
Vayona Narekuli <vayona.narekuli4@gmail.com>
2024-04-24 22:16:30 +0200

    get_NTLmasked updated to allow non country masks

--- R/get_NTLrastermasked.R
+++ R/get_NTLrastermasked.R
@@ -5,6 +5,7 @@
 #' @importFrom geodata gadm
 #' @importFrom sf st_as_sf
 #' @param country country of interest
+#' @param sf_mask Optional parameter, if not NULL, uses the given sf object to mask
 #' @param bearer Your personal bearer token from NASA Earthdata
 #' @param product_id VNP46A1, VNP46A2, VNP46A3, VNP46A4
 #' @param ymd Year or Y-M-D
@@ -13,7 +14,7 @@
 #' @export
 #' @examples
 #' get_NTLrastermasked("NLD", "examplebearertoken", "VNP46A4", 2015, "FALSE")
-get_NTLrastermasked <- function(country = "NLD", bearer, product_id, ymd, output_raster = TRUE) {
+get_NTLrastermasked <- function(country = "NLD", sf_mask = NULL, bearer, product_id, ymd, output_raster = TRUE) {
 
   roi_sf <-  gadm(country = country, level=1, path = tempdir()) |> st_as_sf()
 
@@ -22,7 +23,13 @@ get_NTLrastermasked <- function(country = "NLD", bearer, product_id, ymd, output
                  date = as.character(ymd),
                  bearer = bearer,
                  check_all_tiles_exist = FALSE)
-  r <- r |> mask(roi_sf)
+
+  if (is.null(sf_mask)) {
+    r <- r |> mask(roi_sf)
+  } else {
+    r <- r |> mask(sf_mask)
+  }
+
   r <- r |> rasterToPoints(spatial = TRUE) |>
     as.data.frame()
   names(r) <- c("value", "x", "y")
