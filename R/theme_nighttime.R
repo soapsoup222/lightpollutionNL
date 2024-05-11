eb98a64821ac89ee7bde110616a4dd2d8c813123
Vayona Narekuli <vayona.narekuli4@gmail.com>
2024-05-11 12:55:39 +0200

    theme update

--- R/theme_nighttime.R
+++ R/theme_nighttime.R
@@ -1,12 +1,11 @@
 #' Add nighttime theme to plot
 #' @import ggplot2
-#' @param plot a ggplot object
 #' @returns A plot object with theme applied
 #' @export
 #' @examples
 #' plot + theme_nighttime
 #'
-theme_nighttime <- function(plot){
+theme_nighttime <- function(){
     scale_fill_gradientn(colours = c("#070e34", "#ffe605", "white"),
                          values = c(0, 0.5, 1))+
     theme_void()
--- R/theme_pastelheatmap.R
+++ R/theme_pastelheatmap.R
@@ -1,12 +1,11 @@
 #' Add pastelheatmap theme to plot
 #' @import ggplot2
-#' @param plot a ggplot object
 #' @returns A plot object with theme applied
 #' @export
 #' @examples
 #' plot + theme_pastelheatmap
 #'
-theme_pastelheatmap <- function(plot){
+theme_pastelheatmap <- function(){
     scale_fill_gradientn(colours = c("#668ec2", "#b7deec", "#fbf8c4", "#fbb07a", "#d9534c", "#982fb5"),
                          values = c(0, 0.1, 0.2, 0.3, 0.5, 1))+
     theme_void()
